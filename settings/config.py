546b7e186d2ae144c361ec28ad42e72b103938c9
uvcreation <uttamvs031@gmail.com>
2025-06-23 22:12:47 -0500

    Added setting for apify

---
+++ settings/config.py
@@ -0,0 +1,14 @@
+import os
+
+# Upwork credentials
+UPWORK_USER_NAME = os.getenv("UPWORK_USER_NAME")
+UPWORK_USERNAME = os.getenv("UPWORK_USERNAME")
+UPWORK_PASSWORD = os.getenv("UPWORK_PASSWORD")
+
+
+# Chrome driver settings
+CHROME_VERSIONS = [
+    90,
+    123,
+]
+MAX_ATTEMPTS = 3
